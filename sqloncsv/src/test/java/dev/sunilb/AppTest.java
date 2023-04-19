b85f370783f67b3a3f2aec12d0b63bae49932905
Sunil Bhatia <sunilbhatia79@gmail.com>
2023-04-19 18:47:30 +0530

    Initial project setup with deps resolution

---
+++ sqloncsv/src/test/java/dev/sunilb/AppTest.java
@@ -0,0 +1,13 @@
+package dev.sunilb;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void initialTest() {
+        assertEquals(1, 1);
+    }
+
+}
